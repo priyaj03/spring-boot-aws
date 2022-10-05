ad5d3788397d11ff9d2c8d6d23a1ef8d43532091
priyaj03 <priyajayaraman01@gmail.com>
2022-10-05 19:24:25 +0530

    spring boot aws

---
+++ src/main/java/com/example/springbootwithdocker/SpringbootwithdockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com")
+public class SpringbootwithdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithdockerApplication.class, args);
+	}
+
+}
